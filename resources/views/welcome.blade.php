383967f4f33f377d8819c13793638244414ead2f
conny <connysales@gmail.com>
2024-04-13 12:55:00 +0800

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
 		<div class="row">
 			<div class="col-lg-6 col-md-12 caption-two-panel ml-auto pt-5">
 				<div class="intro-caption mt-5">
-				<h1 class="text-white mb-2">It's not really hard to stand out in crowd</h1>
+					<h1 class="text-white mb-2">Welcome</h1>
+				<h2 class="text-white mb-2">It's not really hard to stand out in crowd</h2>
 				<p class="text-white mb-4"> These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</p>
 			</div>
 		</div>
