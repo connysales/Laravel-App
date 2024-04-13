25a2ad33ea2a9f0611b267ea9ee514cce5b1b7a3
conny <connysales@gmail.com>
2024-04-13 12:48:00 +0800

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,8 @@
 		<div class="row">
 			<div class="col-lg-6 col-md-12 caption-two-panel ml-auto pt-5">
 				<div class="intro-caption mt-5">
-				<h1 class="text-white mb-2">{{ auth()->user()->name }}</h1>
+					<h1 class="text-white mb-2">Welcome</h1>
+				<h2 class="text-white mb-2">{{ auth()->user()->name }}</h2>
 				<p class="text-white mb-4"> These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</p>
 			</div>
 		</div>
